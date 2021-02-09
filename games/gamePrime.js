f652ac1241d22c713592000e33d29d1b3e773d7e
Alexander <a.mozhaev94@gmail.com>
2021-02-09 18:00:53 +0300

    new game prime

---
+++ games/gamePrime.js
@@ -0,0 +1,23 @@
+import readlineSync from 'readline-sync';
+import { correct, tryAgain, congratulations } from '../src/cli.js';
+import { randomNumber } from '../src/random.js';
+import isPrime from '../src/isPrime.js';
+
+const question = () => console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
+export default (nameUser) => {
+  let correctAnswer = 0;
+  question();
+  while (correctAnswer < 3) {
+    const questionNumber = randomNumber(100);
+    console.log(`Question: ${questionNumber}`);
+    const answerUser = readlineSync.question('Your answer: ');
+    if (isPrime(questionNumber) === true && answerUser === 'yes') {
+      correctAnswer += 1;
+      correct();
+    } else if (isPrime(questionNumber) === false && answerUser === 'no') {
+      correctAnswer += 1;
+      correct();
+    } else return tryAgain(nameUser);
+  }
+  congratulations(nameUser);
+};

8fc6204a360618ec4ca4d8300e1ec7e09dcd1e1c
Alexander <a.mozhaev94@gmail.com>
2021-02-09 18:24:53 +0300

    add function welcome

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -2,6 +2,8 @@
 
 import sayMeYourName from '../src/cli.js';
 import gameCalc from '../games/gameCalc.js';
+import { welcome } from '../src/cli.js';
 
+welcome();
 const nameUser = sayMeYourName();
 gameCalc(nameUser);

73c10b1f0b7516029bf32b08a4ef0b2669d060c5
Alexander <a.mozhaev94@gmail.com>
2021-02-09 15:48:19 +0300

    add function randomCollection for game Progression

--- src/random.js
+++ src/random.js
@@ -3,3 +3,20 @@ export const randomOperator = () => {
   const operators = ['+', '-', '*'];
   return String(operators[randomNumber(3)]);
 };
+
+export const randomCollection = () => {
+  let lengthCollection = randomNumber(13);
+  const minimalLength = 5;
+  if (lengthCollection < minimalLength) {
+    lengthCollection = minimalLength;
+  }
+  const result = [];
+  const startNumber = randomNumber(100);
+  const step = randomNumber(30);
+  let itemCollection = startNumber;
+  for (let i = 0; i < lengthCollection; i += 1) {
+    result.push(itemCollection);
+    itemCollection += step;
+  }
+  return result;
+};

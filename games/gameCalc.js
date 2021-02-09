6c565337dfdd91bfa0f9ba0a5b151bb9ce9a88bd
Alexander <a.mozhaev94@gmail.com>
2021-02-09 09:03:26 +0300

    remove gameCheckParityNumber to /games directory

---
+++ games/gameCalc.js
@@ -0,0 +1,40 @@
+import readlineSync from 'readline-sync';
+import { randomNumber, randomOperator } from '../src/random.js';
+import { correct, tryAgain, congratulations } from '../src/cli.js';
+
+const question = () => console.log('What is the result of the expression?');
+export default () => {
+  let correctAnswer = 0;
+  while (correctAnswer < 3) {
+    const firstOperand = randomNumber(100);
+    const secondOperand = randomNumber(100);
+    const operator = randomOperator();
+    question();
+    if (operator === '+') {
+      console.log(`${firstOperand} + ${secondOperand}`);
+      const answerUser = Number(readlineSync.question('Your answer: '));
+      const result = firstOperand + secondOperand;
+      if (result === answerUser) {
+        correctAnswer += 1;
+        correct();
+      } else return tryAgain();
+    } else if (operator === '-') {
+      console.log(`${firstOperand} - ${secondOperand}`);
+      const answerUser = Number(readlineSync.question('Your answer: '));
+      const result = firstOperand - secondOperand;
+      if (result === answerUser) {
+        correctAnswer += 1;
+        correct();
+      } else return tryAgain();
+    } else if (operator === '*') {
+      console.log(`${firstOperand} * ${secondOperand}`);
+      const answerUser = Number(readlineSync.question('Your answer: '));
+      const result = firstOperand * secondOperand;
+      if (result === answerUser) {
+        correctAnswer += 1;
+        correct();
+      } else return tryAgain();
+    }
+  }
+  congratulations();
+};
--- games/gameCheckParityNumber.js
+++ games/gameCheckParityNumber.js
@@ -1,29 +1,29 @@
 import readlineSync from 'readline-sync';
-
-const randomNumber = () => (Math.floor(Math.random() * 100));
+import { randomNumber } from '../src/random.js';
+import { correct, congratulations, tryAgain } from '../src/cli.js';
 
 export default (nameUser) => {
   let correctAnswer = 0;
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
   while (correctAnswer < 3) {
-    const questionNumber = randomNumber();
+    const questionNumber = randomNumber(100);
     console.log(`Question: ${questionNumber}`);
     const answerUser = readlineSync.question('Your answer: ');
     if (questionNumber % 2 === 0 && answerUser === 'yes') {
       correctAnswer += 1;
-      console.log('Correct!');
+      correct();
     } else if (questionNumber % 2 !== 0 && answerUser === 'no') {
       correctAnswer += 1;
-      console.log('Correct!');
+      correct();
     } else if (questionNumber % 2 === 0 && answerUser === 'no') {
-      console.log('"no" is wrong answer ;(. Correct answer was "no".');
-      console.log(`Let's try again, ${nameUser}!`);
+      console.log('"no" is wrong answer ;(. Correct answer was "yes".');
+      tryAgain(nameUser);
       return;
     } else {
       console.log('"yes" is wrong answer ;(. Correct answer was "no".');
-      console.log(`Let's try again, ${nameUser}!`);
+      tryAgain(nameUser);
       return;
     }
   }
-  console.log(`Congratulations, ${nameUser}!`);
+  congratulations(nameUser);
 };
